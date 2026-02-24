78a0bdd90bb756bdb5e88b0c3df21cc3f4c993c5
SpyrosPolemis <spyridon.polemis@my.jcu.edu.au>
2026-02-24 13:20:07 +1000

    Create operate function

--- script.js
+++ script.js
@@ -1,3 +1,22 @@
+let number1;
+let number2;
+let operator;
+
+function operate (number1, number2, operator) {
+    if(operator === "+") {
+        return (add(number1, number2))
+    }
+    if(operator === "-") {
+        return (subtract(number1, number2))
+    }
+    if(operator === "*") {
+        return (multiply(number1, number2))
+    }
+    if(operator === "/") {
+        return (divide(number1, number2))
+    }
+}
+
 function add(number1, number2) {
     return number1 + number2;
 }
